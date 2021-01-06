3a7f75a1f84af687a46f42736e6139c77b8d1105
vasnastos <nastosvasileios99@gmail.com>
2021-01-06 20:39:48 +0200

    ListCtrl

--- Random_Surfer/FORM_GUI/listexample.cpp
+++ Random_Surfer/FORM_GUI/listexample.cpp
@@ -6,46 +6,53 @@ class MyFrame : public wxFrame
     wxListCtrl* m_item_list;
 
 public:
-    MyFrame() : wxFrame(NULL, wxID_ANY,  wxT("Hello wxWidgets"), wxPoint(50,50), wxSize(800,600))
+    MyFrame() : wxFrame(NULL, wxID_ANY,"LIST EXAMPLE")
     {
-        wxPanel* mainPane = new wxPanel(this);
+        this->SetSize(400,400);
         wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
         
-        m_item_list = new wxListCtrl(mainPane, wxTEXT_ALIGNMENT_CENTER, wxPoint(300,300),wxSize(750,200),wxLC_VIRTUAL|wxLC_REPORT);
+        m_item_list = new wxListCtrl(this, wxTEXT_ALIGNMENT_CENTER,wxDefaultPosition,wxSize(200,200),wxLC_REPORT);
         // Add first column       
         wxListItem col0;
         col0.SetId(0);
         col0.SetText( _("Foo") );
-        col0.SetWidth(200);
+        col0.SetWidth(50);
         m_item_list->InsertColumn(0, col0);
         
         // Add second column
         wxListItem col1;
         col1.SetId(1);
         col1.SetText( _("Name") );
-        col1.SetWidth(200);
+        col1.SetWidth(50);
         m_item_list->InsertColumn(1, col1);
         
         // Add third column     
         wxListItem col2;
         col2.SetId(2);
-        col2.SetWidth(200);
+        col2.SetWidth(50);
         col2.SetText( _("Comments") );
         m_item_list->InsertColumn(2, col2);
+        
+        
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                wxListItem it;
+                it.SetId(i+j);
+                it.SetText(_(""));
+                m_item_list->InsertItem(it);
+            }
+        }
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                m_item_list->SetItem(i,j,"row"+std::to_string(j+1));
+            }
+        }
         sizer->Add(m_item_list);
-        mainPane->SetSizer(sizer);
-        m_item_list->InsertItem(0,"COLUMN1");
-        m_item_list->InsertItem(1,"COLUMN2");
-        m_item_list->InsertItem(2,"COLUMN3");
-        m_item_list->SetItem(0,0,"COLUMN1");
-        m_item_list->SetItem(0,1,"COLUMN2");
-        m_item_list->SetItem(0,2,"COLUMN3");
-        m_item_list->SetItem(1,0,"COLUMN1");
-        m_item_list->SetItem(1,1,"COLUMN2");
-        m_item_list->SetItem(1,2,"COLUMN3");
-        m_item_list->SetItem(2,0,"COLUMN1");
-        m_item_list->SetItem(2,1,"COLUMN2");
-        m_item_list->SetItem(2,2,"COLUMN3");
+        this->SetSizer(sizer);
     }
 };
 class MyApp: public wxApp
